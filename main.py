75359981356107311c3b9e84a41d2ee1f536d4c2
Piotr Kafel <peterkaf132@gmail.com>
2023-08-03 16:53:48 +0200

    Added endpoints to access data by date

--- main.py
+++ main.py
@@ -1,51 +1,77 @@
 import pandas as pd
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-# Step 1: Read the .csv dataset
+# Read the .csv dataset
 df = pd.read_csv('warsaw.csv')
 
 
-# Step 2: Define routes that return data from the dataset
+# Define routes that return data from the dataset
 @app.route('/')
 def home():
-    return "Welcome to the Weather API!"
+    return render_template("home.html")
 
 
 @app.route('/weather', methods=['GET'])
 def get_weather():
-    # Step 3: Convert the data to JSON
+    # Convert the data to JSON
     data = df.to_dict(orient='records')
 
-    # Step 4: Return the JSON data from the route
-    return jsonify(data)
-
-
-@app.route('/weather/date/<date>', methods=['GET'])
-def get_weather_by_date(date):
-    # Filter the dataframe based on the specified date
-    filtered_df = df[df['DATE'] == date]
-
-    # Convert the filtered data to JSON
-    data = filtered_df.to_dict(orient='records')
-
     # Return the JSON data from the route
     return jsonify(data)
 
 
-@app.route('/weather/station/<station_id>', methods=['GET'])
-def get_weather_by_station(station_id):
-    # Filter the dataframe based on the specified station ID
-    filtered_df = df[df['STATION'] == station_id]
+class DateAPI:
+    @staticmethod
+    @app.route('/weather/date/<date>', methods=['GET'])
+    def get_weather_by_date(date):
+        # Filter the dataframe based on the specified date
+        filtered_df = df[df['DATE'] == date]
 
-    # Convert the filtered data to JSON
-    data = filtered_df.to_dict(orient='records')
+        # Convert the filtered data to JSON
+        data = filtered_df.to_dict(orient='records')
 
-    # Return the JSON data from the route
-    return jsonify(data)
+        # Return the JSON data from the route
+        return jsonify(data)
+
+    @staticmethod
+    @app.route('/weather/date/before/<date_before>', methods=['GET'])
+    def get_weather_by_date_before(date_before):
+        # Filter the dataframe based on the specified date
+        filtered_df = df[df['DATE'] <= date_before]
+
+        # Convert the filtered data to JSON
+        data = filtered_df.to_dict(orient='records')
+
+        # Return the JSON data from the route
+        return jsonify(data)
+
+    @staticmethod
+    @app.route('/weather/date/after/<date_after>', methods=['GET'])
+    def get_weather_by_date_after(date_after):
+        # Filter the dataframe based on the specified date
+        filtered_df = df[df['DATE'] >= date_after]
+
+        # Convert the filtered data to JSON
+        data = filtered_df.to_dict(orient='records')
+
+        # Return the JSON data from the route
+        return jsonify(data)
+
+    @staticmethod
+    @app.route('/weather/date/between/<date_after>/<date_before>', methods=['GET'])
+    def get_weather_by_date_range(date_after, date_before):
+        # Filter the dataframe based on the specified date range
+        filtered_df = df[(df['DATE'] >= date_after) & (df['DATE'] <= date_before)]
+
+        # Convert the filtered data to JSON
+        data = filtered_df.to_dict(orient='records')
+
+        # Return the JSON data from the route
+        return jsonify(data)
 
 
-# Step 5: Run the app
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
